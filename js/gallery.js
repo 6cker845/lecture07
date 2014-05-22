c94197c3eeb234ea5766a23d9b7a1b1037373eab
6cker845 <nick_fun6king@yahoo.co.jp>
2014-05-22 12:28:39 +0900

    課題2 コメントを記入

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,6 @@
 var photoListElement = document.querySelector("#photos");
 
+
 var photoList = [
     {src: "img/apple.png", title: "apple"},
     {src: "img/facebook.png", title: "facebook"},
@@ -8,11 +9,13 @@ var photoList = [
     {src: "img/tumblr.png", title: "前"},
     {src:	"img/twitter.png", title: "横"}
 ];
+//画像を用意する。
 
 var isReady = function(){
     return photoListElement != null &&
         photoList != null &&
         photoList.length > 0;
+        //photoListElementが存在し、photoListに要素が入っていてる場合、isReady()はture.
 };
 
 var renderPhoto = function(index){
@@ -21,6 +24,7 @@ var renderPhoto = function(index){
     elm.setAttribute("src", photo.src);
     elm.setAttribute("title", photo.title);
     return elm;
+    //画像の画像ファイルとタイトルのエレメントを作成している。
 };
 
 var showPhotos = function(){
@@ -31,6 +35,8 @@ var showPhotos = function(){
             photoListElement.appendChild(elm);
             index = index + 1;
         }
+        //index値がphotoList.lengthより小さい場合、新しく画像を追加する。
+        
         // whileは中身がtureになっている間、処理が繰り返される。
         // while文は終わるように書かなければ、処理が無限に行われてブラウザが固まる。そこは注意しなきゃいけない。
     }
@@ -39,6 +45,7 @@ var showPhotos = function(){
 var initApp = function(){
     var btn = document.querySelector("#startButton");
     btn.addEventListener("click", showPhotos);
+    //ボタンをクリックしたらshowPhotos関数を呼び出す
 };
 
 initApp();
